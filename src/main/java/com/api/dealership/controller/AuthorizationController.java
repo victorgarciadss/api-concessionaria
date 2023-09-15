b97c94007ae6bbf3e6f5af91ac59f7297e051ca8
victorgarciadss <garciavictor.1308@gmail.com>
2023-09-15 20:47:05 -0300

    Refactor: change logic to Service class

--- src/main/java/com/api/dealership/controller/AuthorizationController.java
+++ src/main/java/com/api/dealership/controller/AuthorizationController.java
@@ -5,6 +5,7 @@ import com.api.dealership.dto.AuthorizationDto;
 import com.api.dealership.dto.RegisterDto;
 import com.api.dealership.entity.logins.Login;
 import com.api.dealership.repository.LoginRepository;
+import com.api.dealership.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,34 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorizationController {
 
     @Autowired
-    private LoginRepository loginRepository;
+    private AuthorizationService authorizationService;
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private TokenService tokenService;
-
     @PostMapping(value = "/cadastro")
     public Login registerUser(@RequestBody RegisterDto registerDto){
-        UserDetails user = loginRepository.findByUserName(registerDto.userName());
-
-        if(user != null){
-            throw new IllegalArgumentException("Usuário já está cadastrado");
-        }
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDto.password());
-        Login newUser = new Login(registerDto.userName(), encryptedPassword, registerDto.role());
-
-        return loginRepository.save(newUser);
+        return authorizationService.registerUser(registerDto);
     }
 
     @PostMapping(value = "/login")
     public ResponseEntity<String> makeLogin(@RequestBody AuthorizationDto authorizationDto){
-        UsernamePasswordAuthenticationToken dataHash = new UsernamePasswordAuthenticationToken(authorizationDto.userName(), authorizationDto.password());
-        Authentication auth = authenticationManager.authenticate(dataHash);
-
-        String token = tokenService.generateToken((Login) auth.getPrincipal());
+        String token = authorizationService.makeLogin(authorizationDto, authenticationManager);
 
         return ResponseEntity.ok().body(token);
     }
--- src/main/java/com/api/dealership/service/AuthorizationService.java
+++ src/main/java/com/api/dealership/service/AuthorizationService.java
@@ -1,10 +1,18 @@
 package com.api.dealership.service;
 
+import com.api.dealership.config.security.TokenService;
+import com.api.dealership.dto.AuthorizationDto;
+import com.api.dealership.dto.RegisterDto;
+import com.api.dealership.entity.logins.Login;
 import com.api.dealership.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +21,31 @@ public class AuthorizationService implements UserDetailsService {
     @Autowired
     private LoginRepository loginRepository;
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return loginRepository.findByUserName(username);
+    }
+
+    public Login registerUser(RegisterDto data){
+        UserDetails user = loginRepository.findByUserName(data.userName());
+
+        if(user == null){
+            throw new IllegalArgumentException("Usuário já está cadastrado");
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        Login newUser = new Login(data.userName(), encryptedPassword, data.role());
+
+        return loginRepository.save(newUser);
+    }
+
+    public String makeLogin(AuthorizationDto auth, AuthenticationManager manager){
+        UsernamePasswordAuthenticationToken credentials = new UsernamePasswordAuthenticationToken(auth.userName(), auth.password());
+        Authentication authentication = manager.authenticate(credentials);
+
+        return tokenService.generateToken((Login) authentication.getPrincipal());
     }
 }
